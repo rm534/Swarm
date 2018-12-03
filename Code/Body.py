7e18600bcb6ccd6f18f89c75d7e75d9a0f4ef258
Robin <robinmatzner@live.co.uk>
2018-12-03 12:20:49 +0000

    Added preliminary functions to Body.py

--- Code/Body.py
+++ Code/Body.py
@@ -5,3 +5,16 @@ class SwarmBody():
     def __init__(self):
         return True
 
+    #TODO Write the movement functions and design lidar implementation
+
+    def move_forward(self):
+        return
+    def move_backward(self):
+        return
+    def move_right(self):
+        return
+    def move_left(self):
+        return
+    def get_lidar(self):
+        return
+
