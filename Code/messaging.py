9de9c7150a98b2d5fdd4a79952897641f0c5ca6b
Robin <robinmatzner@live.co.uk>
2018-12-04 12:29:03 +0000

    safety commit

--- Code/messaging.py
+++ Code/messaging.py
@@ -5,4 +5,9 @@ import time
 import ustruct
 
 class Messaging(object):
+    def __init__(self):
+        return
 
+    def mqtt_connect(self):
+        try:
+            self.client = MQTTClient("de
