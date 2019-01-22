43627cbd02aa531a4ef8eb99d2dddc24be38b7fe
Robin <robinmatzner@live.co.uk>
2019-01-22 18:19:00 +0000

    Added PWN and speed/distance functionality to motor code. Added rotation code to motor Added Temperature sensor code Added new motor test code for updated functions TODO: Solar Panel VD code Battery Monitor

--- Code/Body.py
+++ Code/Body.py
@@ -1,20 +1,30 @@
 import pycom
 import time
+import machine
 from machine import Pin
 from machine import I2C
 from machine import Timer
+from micropython import const
+from machine import PWM
 import mpu6050
+import sys
+
+I2C_BUS_0 = const(0)
+I2C_BUS_1 = const(1)
 
 
 class SwarmBody():
-    def __init__(self, motor_1F='P5', motor_1B='P6', motor_2F='P7', motor_2B='P8', lidar_SDA='P9', lidar_SCL='P10',
-                 lidar_DIO1='P19', lidar_DIO2='P20', lidar_DIO3='P21', lidar_DIO4='P22', gyro_SDA='P11', gyro_SCL='P12',
+    def __init__(self, motor_pin1='P10', motor_pin2='P11', motor_pin3='P12', motor_pin4='P6', lidar_SDA='P9',
+                 lidar_SCL='P8',
+                 lidar_DIO1='P19', lidar_DIO2='P20', lidar_DIO3='P21', lidar_DIO4='P22', gyro_SDA='P7', gyro_SCL='P5',
                  temp_AD='P13'):
-        self.initialise_motor(motor_1F, motor_1B, motor_2F, motor_2B)
+        self.initialise_motor(motor_pin1, motor_pin2, motor_pin3, motor_pin4)
         self.initialise_gyro(gyro_SDA, gyro_SCL)
         self.initialise_lidar(lidar_SDA, lidar_SCL, lidar_DIO1, lidar_DIO2, lidar_DIO3, lidar_DIO4)
+        self.initialise_temp(temp_AD)
 
-    # TODO: initialise temperature sensor
+    # TODO: Solar Panel VD code
+    # TODO: Battery Monitor code
     def initialise_lidar(self, lidar_SDA, lidar_SCL, lidar_DIO1, lidar_DIO2, lidar_DIO3, lidar_DIO4,
                          lidar_baudrate=20000):
         self.lidar_SDA = lidar_SDA
@@ -24,48 +34,77 @@ class SwarmBody():
         self.lidar_DIO2 = Pin(lidar_DIO2, mode=Pin.OUT)
         self.lidar_DIO3 = Pin(lidar_DIO3, mode=Pin.OUT)
         self.lidar_DIO4 = Pin(lidar_DIO4, mode=Pin.OUT)
-        self.lidar_I2C = I2C(1, pins=(self.lidar_SDA, self.lidar_SCL))
-        self.lidar_I2C.init(I2C.MASTER, lidar_baudrate)
+        self.lidar_I2C = I2C(I2C_BUS_1, pins=(self.lidar_SDA, self.lidar_SCL))
+        self.lidar_I2C.init(I2C.MASTER, self.lidar_baudrate)
 
-    def initialise_motor(self, motor_1F, motor_1B, motor_2F, motor_2B):
-        self.motor_1F = Pin(motor_1F, mode=Pin.OUT)
-        self.motor_1B = Pin(motor_1B, mode=Pin.OUT)
-        self.motor_2F = Pin(motor_2F, mode=Pin.OUT)
-        self.motor_2B = Pin(motor_2B, mode=Pin.OUT)
+    def initialise_motor(self, motor_pin1, motor_pin2, motor_pin3, motor_pin4):
+        self.motor_pin1 = motor_pin1
+        self.motor_pin2 = motor_pin2
+        self.motor_pin3 = motor_pin3
+        self.motor_pin4 = motor_pin4
+        self.motor_PWM = PWM(0, frequency=500)
+        self.duty_cycle = 0.7
+        self.motor_stop()
 
     def initialise_gyro(self, gyro_SDA, gyro_SCL, gyro_baudrate=20000):
         self.gyro_SDA = gyro_SDA
         self.gyro_SCL = gyro_SCL
-        self.gyro_I2C = I2C(0, pins=(self.gyro_SDA, self.gyro_SCL))
-        self.gyro_I2C.init(I2C.MASTER, baudrate=gyro_baudrate)
+        self.gyro_baudrate = gyro_baudrate
+        self.gyro_I2C = I2C(I2C_BUS_0, pins=(self.gyro_SDA, self.gyro_SCL))
+        self.gyro_I2C.init(I2C.MASTER, baudrate=self.gyro_baudrate)
         self.gyro = mpu6050.accel(self.gyro_I2C)
 
-    def move_forward(self, dt):
-        self.motor_1F.value(1)
-        self.motor_2F.value(1)
-        time.sleep(dt)
-        self.motor_1F.value(0)
-        self.motor_2F.value(0)
+    def initialise_temp(self, temp_AD):
+        self.temp_ADC = machine.ADC()
+        self.temp_AD = self.temp_ADC.channel(pin=temp_AD)
+
+    def motor_stop(self):
+        Pin(self.motor_pin1, mode=Pin.OUT).value(0)  # Pin1
+        Pin(self.motor_pin2, mode=Pin.OUT).value(0)  # Pin2
+        Pin(self.motor_pin3, mode=Pin.OUT).value(0)  # Pin3
+        Pin(self.motor_pin4, mode=Pin.OUT).value(0)  # Pin4
+
+    def move_forward(self, d, speed):
+        self.motor_stop()
+        self.motor_PWM.channel(0, pin=self.motor_pin1, duty_cycle=self.duty_cycle)
+        self.motor_PWM.channel(0, pin=self.motor_pin3, duty_cycle=self.duty_cycle)
+        T = d / speed
+        time.sleep(T)
+        self.motor_stop()
         return
 
-    def move_backward(self, dt):
-        self.motor_1B.value(1)
-        self.motor_2B.value(1)
-        time.sleep(dt)
-        self.motor_1B.value(0)
-        self.motor_2B.value(0)
+    def move_backward(self, d, speed):
+        self.motor_stop()
+        self.motor_PWM.channel(0, pin=self.motor_pin2, duty_cycle=self.duty_cycle)
+        self.motor_PWM.channel(0, pin=self.motor_pin4, duty_cycle=self.duty_cycle)
+        T = d / speed
+        time.sleep(T)
+        self.motor_stop()
         return
 
-    def move_right(self):
+    def rotate_clockwise(self, angle, rotational_speed):
+        self.motor_stop()
+        self.motor_PWM.channel(0, pin=self.motor_pin1, duty_cycle=self.duty_cycle)
+        self.motor_PWM.channel(0, pin=self.motor_pin4, duty_cycle=self.duty_cycle)
+        T = angle / rotational_speed
+        time.sleep(T)
+        self.motor_stop()
         return
 
-    def move_left(self):
+    def rotate_anti_clockwise(self, angle, rotational_speed):
+        self.motor_stop()
+        self.motor_PWM.channel(0, pin=self.motor_pin2, duty_cycle=self.duty_cycle)
+        self.motor_PWM.channel(0, pin=self.motor_pin3, duty_cycle=self.duty_cycle)
+        T = angle / rotational_speed
+        time.sleep(T)
+        self.motor_stop()
         return
 
-    # TODO Write the movement functions and design lidar implementation
+    # TODO: Write lidar function
     def get_lidar(self):
         return
 
+    # TODO: Write angle calculation funciton
     def get_angle(self):
         # get analog read of gyro minus the zero voltage point
         # Divide this by the gyro sensitivity
@@ -77,15 +116,36 @@ class SwarmBody():
         print("[+] Acc Reading: ", acc)
         return acc
 
+    def get_temp(self):
+        temp = self.temp_AD()
+        print("[+] Temp Reading: ", temp)
+        return temp
+
     def test_timer(self):
-        gyro_timer = Timer.Alarm(self.get_gyro, s=2, periodic=True)
-        lidar_timer = Timer.Alarm(self.get_lidar, s=2, periodic=True)
+        self.gyro_timer = Timer.Alarm(self.get_gyro, ms=300, periodic=True)
+        self.lidar_timer = Timer.Alarm(self.get_lidar, s=2, periodic=True)
+        self.temp_timer = Timer.Alarm(self.get_temp, s=2, periodic=True)
 
     def test_motor(self):
-        self.move_forward(1)
-        self.move_backward(1)
+        self.move_forward(1, 1)
+        self.move_backward(1, 1)
+        self.rotate_clockwise(90, 2)
+        self.rotate_anti_clockwise(90, 2)
+
+    def stop(self):
+        self.gyro_timer.cancel()
+        self.lidar_timer.cancel()
+        self.temp_timer.cancel()
+        self.motor_stop()
+        sys.exit()
 
 
 if __name__ == '__main__':
     body = SwarmBody()
-    body.test_timer()
+
+    try:
+        body.test_timer()
+    except:
+        print("[-] Error!")
+        print("[-] Exiting Immediately")
+        body.stop()
